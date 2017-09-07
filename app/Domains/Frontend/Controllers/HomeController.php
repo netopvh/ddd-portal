a12c74cb9b66c0b796d93d8cafe4f9067a277414
Angelo Neto <netopvh@gmail.com>
2017-09-07 13:37:31 +0000

    Redirecionamento Home para Admin

--- app/Domains/Frontend/Controllers/HomeController.php
+++ app/Domains/Frontend/Controllers/HomeController.php
@@ -14,6 +14,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('themes.layout.master');
+        return redirect()->route('admin.home');
     }
 }
